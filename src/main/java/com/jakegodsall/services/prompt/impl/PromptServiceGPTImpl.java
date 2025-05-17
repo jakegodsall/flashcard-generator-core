8db7d6361215146376cb0d4d7aa29250f4f3e516
jakegodsall <jake.edward.godsall@gmail.com>
2025-05-17 20:26:40 +0100

    added generate multiple flashcards for word functionality

--- src/main/java/com/jakegodsall/services/prompt/impl/PromptServiceGPTImpl.java
+++ src/main/java/com/jakegodsall/services/prompt/impl/PromptServiceGPTImpl.java
@@ -41,6 +41,16 @@ public class PromptServiceGPTImpl implements PromptService {
     }
 
     @Override
+    public String generatePromptForMultipleFlashcards(String targetWord, FlashcardType flashcardType, Language language, Options options, int count) {
+        return "Generate " + count + " different flashcards as a JSON array. For each flashcard in the array:\n" +
+               generateBasePrompt() + getDescriptionOfContent(flashcardType) + 
+               "The structure for each flashcard in the array should be:\n" + 
+               getFlashcardStructure(flashcardType) + 
+               "The word is " + targetWord + " and the target language is " + language.getName() + ".\n" +
+               "Each flashcard should use different example sentences and translations while maintaining accuracy.\n";
+    }
+
+    @Override
     public String generatePromptForSubsequentWord(String targetWord) {
         return "The next word is " + targetWord;
     }
