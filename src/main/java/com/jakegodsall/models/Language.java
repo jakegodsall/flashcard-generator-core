d3fe6c97555ac9a217a85d526d972fdd982e5c62
jakegodsall <jake.edward.godsall@gmail.com>
2025-12-20 12:41:19 +0000

    remove verbose method

--- src/main/java/com/jakegodsall/models/Language.java
+++ src/main/java/com/jakegodsall/models/Language.java
@@ -25,12 +25,4 @@ public class Language {
     public String toString() {
         return this.name;
     }
-
-    public String toStringVerbose() {
-        return "(name=" + this.name
-                + ", supportsStress=" + this.isSupportsStress()
-                + ", tenses=" + this.getTenses()
-                + ", genders=" + this.getGenders()
-                + ")";
-    }
 }
